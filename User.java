e1e6bc121886f36f4fdefc562187ac8b89bfd019
nordonezc <nordonezc@unal.edu.co>
2015-09-16 22:33:49 -0500

    Completo getters y setters de la clase User
    Se añaden funciones para cambiar o ver desde una clase externa los datos
    de user, ademas de corregir el tipo de funcion de getter_name.

--- User.java
+++ User.java
@@ -23,7 +23,45 @@ public abstract class User
 		this.age = age;
 	}
 
-	public static void getter_name(){
+	public static String getter_name(){
 		return this.name;
 	}
+
+	public static void setter_name(String new_name){
+		this.name = new_name;
+	}
+
+	public static String getter_lastname(){
+		return this.lastname;
+	}
+
+	public static void setter_lastname(String new_lastname){
+		this.lastname = new_lastname;
+	}
+
+	public static String getter_username(){
+		return this.username;
+	}
+
+	public static void setter_username(String new_username){
+		this.username = new_username;
+	}
+
+	public static String getter_password(){
+		return this.password;
+	}
+
+	public static void setter_password(String new_password){
+		this.password = new_password;
+	}
+
+	public static int getter_age(){
+		return this.age;
+	}
+
+	public static void setter_age(String new_age){
+		this.age = new_age;
+	}	
+
+
 }
