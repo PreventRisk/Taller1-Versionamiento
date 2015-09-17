96270c3ec3487a68e459ea57f138bb65ec734c3d
idvanegasp <iVanMac@MacBook-de-Ivan.local>
2015-09-16 20:33:38 -0500

    adding files

---
+++ Post.java
@@ -0,0 +1,60 @@
+
+import java.util.Date;
+/**
+ *
+ * @author monica
+ */
+public class Post {
+    private String topic;
+    private Date dateCreated;
+    private Date lastupdate;
+    private boolean isAllowed;
+
+    public Post(String topic, Date dateCreated, Date lastupdate, boolean isAllowed) {
+        this.topic = topic;
+        this.dateCreated = dateCreated;
+        this.lastupdate = lastupdate;
+        this.isAllowed = isAllowed;
+    }
+
+   
+    public void comment(){}
+    public void rate(){}
+    public void share(){}
+   
+    public String getTopic() {
+        return topic;
+    }
+
+    public Date getDateCreated() {
+        return dateCreated;
+    }
+
+    public Date getLastupdate() {
+        return lastupdate;
+    }
+
+    public boolean isIsAllowed() {
+        return isAllowed;
+    }
+
+    public void setTopic(String topic) {
+        this.topic = topic;
+    }
+
+    public void setDateCreated(Date dateCreated) {
+        this.dateCreated = dateCreated;
+    }
+
+    public void setLastupdate(Date lastupdate) {
+        this.lastupdate = lastupdate;
+    }
+
+    public void setIsAllowed(boolean isAllowed) {
+        this.isAllowed = isAllowed;
+    }
+    
+    
+    
+}
+
---
+++ Regular.java
@@ -0,0 +1,22 @@
+/* package whatever; // don't place package name! */
+// @Carlos
+
+
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+/* Name of the class has to be "Main" only if the class is public. */
+class Regular
+{
+	private static int postViews;
+	private static int strikesNumber;
+	private static int starsNumber;
+	
+	public Regular(int postViews, int StrikesNumber, int starsNumber){
+		this.postViews = postViews;
+		this.strikesNumber = strikesNumber;
+		this.starsNumber = starsNumber;
+		
+	}
+	
