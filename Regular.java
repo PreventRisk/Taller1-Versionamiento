aadb212e1e96d34374562bbbdebdc80fe0bfa6dd
Carlitoshsh <caragomezher@unal.edu.co>
2015-09-16 22:10:31 -0500

    Edit Regular class

--- Regular.java
+++ Regular.java
@@ -1,22 +1,19 @@
-/* package whatever; // don't place package name! */
-// @Carlos
-
+/* 
+* @author Carlos 
+*
+*/
 
 import java.util.*;
-import java.lang.*;
-import java.io.*;
 
-/* Name of the class has to be "Main" only if the class is public. */
-class Regular
-{
+class Regular {
 	private static int postViews;
 	private static int strikesNumber;
 	private static int starsNumber;
-	
-	public Regular(int postViews, int StrikesNumber, int starsNumber){
+
+	public Regular(int postViews, int StrikesNumber, int starsNumber) {
 		this.postViews = postViews;
 		this.strikesNumber = strikesNumber;
 		this.starsNumber = starsNumber;
-		
+
 	}
-	
+}
