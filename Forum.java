bcceb13247d731640d562a1ffa85cd85ae782943
idvanegasp <iVanMac@MacBook-de-Ivan.local>
2015-09-16 20:41:36 -0500

    add forum

---
+++ Forum.java
@@ -0,0 +1,31 @@
+package software2;
+
+import java.util.Date;
+
+public class Forum {
+	
+	private String name="";
+	private Date dateCreated;
+	private String category = "";
+	
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setDateCreated(Date dateCreated) {
+		this.dateCreated = dateCreated;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+
+	public Forum(String n, Date d, String c){
+		setName(n);
+		setDateCreated(d);
+		setCategory(c);
+		
+	}
+	
+}
